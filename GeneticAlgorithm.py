3033944912a280b63d2526095bba194d279da311
JS <hjsarmiento1999@gmail.com>
2026-01-03 18:28:23 +0100

    Added grid for checking the best first three results and different parameters, plotting graphic generation vs conflicts, time and configuration. The grid is saved in a file, to have a better discussion about the different parameters used

--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -2,7 +2,8 @@ import random
 import networkx as nx
 import os
 import matplotlib.pyplot as plt
-
+import time
+import pandas as pd
 
 class GeneticAlgorithm:
     def __init__(
@@ -15,8 +16,8 @@ class GeneticAlgorithm:
         ELITISM_SIZE = 3,
         TOURNAMENT_SIZE = 3,
         SELECTION_METHOD = 'tournament',
-        CROSSOVER_METHOD = 'uniform'
-        MUTATE_METHOD = 'independent'
+        CROSSOVER_METHOD = 'uniform',
+        MUTATE_METHOD = 'independent',
     ):
         self.filename = filename
         # This is our k parameter, to choose the min num of colors 
@@ -35,6 +36,9 @@ class GeneticAlgorithm:
         self.NUM_VERTICES = 0 # default value
         self.edges = 0 # default value
         self.solution = []
+
+        self.elapsed_time = 0
+        self.generations_conflicts = []
 
     
     # DIMACS .col file reader
@@ -174,25 +178,25 @@ class GeneticAlgorithm:
         n = len(p1)
         p1_idx, p2_idx = sorted(random.sample(range(n), 2))
     
-        child1 = (
+        c1 = (
             p1[:p1_idx]
             + p2[p1_idx:p2_idx]
             + p1[p2_idx:]
         )
     
-        child2 = (
+        c2 = (
             p2[:p1_idx]
             + p1[p1_idx:p2_idx]
             + p2[p2_idx:]
         )
     
-        return child1, child2
+        return c1, c2
     
     def crossover(self, p1, p2):
         if self.CROSSOVER_METHOD == "two_point":
-            return two_point_crossover(p1, p2)
+            return self.two_point_crossover(p1, p2)
         elif self.CROSSOVER_METHOD == "uniform":
-            return uniform_crossover(p1, p2)
+            return self.uniform_crossover(p1, p2)
         else:
             raise ValueError("Invalid crossover method")
 
@@ -211,20 +215,20 @@ class GeneticAlgorithm:
         chromosome[i] = new_color
         return chromosome
         
-    def mutate_independent(self, chromosome):
+    def mutate_independent(self, individual):
         for i in range(self.NUM_VERTICES):
             if random.random() < self.MUTATION_RATE:
-                old_color = chromosome[i]
-                chromosome[i] = random.choice(
+                old_color = individual[i]
+                individual[i] = random.choice(
                     [c for c in range(self.NUM_COLORS) if c != old_color]
                 )
-        return chromosome
+        return individual
         
     def mutate(self, chromosome):
         if self.MUTATE_METHOD == "one":
-            self.mutate_one_gene(chromosome)
+            return self.mutate_one_gene(chromosome)
         elif self.MUTATE_METHOD == "independent":
-            sellf.mutate_independent(chromosome)
+            return self.mutate_independent(chromosome)
         else:
             raise ValueError("Invalid mutation method")
 
@@ -269,12 +273,54 @@ class GeneticAlgorithm:
         print(self.solution)
         print("Conflicts:", self.count_conflicts(self.solution))
         print("Number of Colors", set(self.solution))
+        print(f"The program took {self.elapsed_time} seconds to run.")
+        #self.plot_conflicts()
 
+
+    def plot_conflicts(self):
+        if not self.generations_conflicts:
+            print("No data to plot.")
+            return
+    
+        # 1. Extract data
+        gens, confs = zip(*self.generations_conflicts)
+    
+        # 2. Create the plot
+        plt.style.use('seaborn-v0_8-darkgrid') # Optional: makes it look modern
+        fig, ax = plt.subplots()
+        
+        ax.plot(gens, confs, label='Fitness (Conflicts)', color='#2ca02c', linewidth=2)
+        
+        # 3. Add details
+        ax.set_title('Evolutionary Progress', fontsize=14)
+        ax.set_xlabel('Generation')
+        ax.set_ylabel('Best Conflict Score')
+        ax.legend()
+    
+        plt.show()
 
     def genetic_algorithm(self):
+        start_time = time.time()
+        
         self.read_col_file()
         population = self.initialize_population(self.NUM_COLORS)
+
     
+        
+        data = [
+            {
+                'filename': self.filename,
+                'POPULATION_SIZE': self.POPULATION_SIZE,
+                'GENERATIONS': self.GENERATIONS,
+                'MUTATION_RATE': self.MUTATION_RATE,
+                'ELITISM_SIZE': self.ELITISM_SIZE,
+                'TOURNAMENT_SIZE': self.TOURNAMENT_SIZE,
+                'SELECTION_METHOD': self.SELECTION_METHOD,
+                'CROSSOVER_METHOD': self.CROSSOVER_METHOD,
+                'MUTATE_METHOD': self.MUTATE_METHOD,
+            }
+        ]
+        
         for generation in range(self.GENERATIONS):
             new_population = []
 
@@ -297,27 +343,40 @@ class GeneticAlgorithm:
             population.sort(key=self.fitness, reverse=True)
             elites = population[:self.ELITISM_SIZE]
             new_population[:self.ELITISM_SIZE] = elites
-    
+            
             population = new_population
-    
             best = max(population, key=self.fitness)
             conflicts = self.count_conflicts(best)
-    
+
+            self.generations_conflicts.append([generation, conflicts])
             if conflicts == 0:
                 print(f"Valid coloring found at generation {generation}")
                 print(f"Gen {generation}: best conflicts = {conflicts}")
 
                 
-                G = self.build_nx_graph()
-                self.draw_colored_graph(G, best)
+                #G = self.build_nx_graph()
+                #self.draw_colored_graph(G, best)
                 self.solution = best
                 self.print_solution()
-                return
+                
+                end_time = time.time()
+                self.elapsed_time = end_time - start_time
+                data.append(self.elapsed_time)
+                data.append(self.NUM_COLORS)
+                data.append(conflicts)
+                data.append(generation)
+                data.append(self.generations_conflicts)
+                data.append(best)
+                return data
     
             if generation % 50 == 0:
                 print(f"Gen {generation}: best conflicts = {conflicts}")
                 
         self.solution = max(population, key=self.fitness)
-        
-
-
+        end_time = time.time()
+        self.elapsed_time = end_time - start_time
+        data.append('NO_SOLUTION')
+        data.append(self.NUM_COLORS)
+        data.append(conflicts)
+        data.append(generation)
+        return data
